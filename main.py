8334b61c96751f6105bfa17f6f1027ae13ec8a32
ankit <ankit.f.parmar@gmail.com>
2019-11-30 20:51:42 +0530

    reduced encoding size to fit the network in gpu memoru

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from siamese_net import SiameseNet
 def main():
     dataset_path = '../../datasets/monkey_species/'
     learning_rate = 10e-4
-    batch_size = 4
+    batch_size = 8
 
     tensorboard_logs_path = './logs/'
 
+    print('Initialising Siamese Network...')
     net = SiameseNet(dataset_path, learning_rate, batch_size, tensorboard_logs_path)
 
     evaluate_each = 1000
     number_of_train_iterations = 10000
 
+    print('Training Siamese Network...')
     evaluation_accuracy = net.train_siamese_net(number_of_train_iterations, evaluate_each,
                                                 'siamese_net')
 
